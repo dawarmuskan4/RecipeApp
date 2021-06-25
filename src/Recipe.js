03a3dbef47d7fbe969a6c2cc7443afbdaadb2c66
Muskan Dawar <sanchitdawar34@gmail.com>
2021-06-25 18:36:13 +0530

    added

---
+++ src/Recipe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const recipe=({title,calories,image})=>{
+
+
+    return(
+        <div>
+            <h1>{title}</h1>
+            <p>{calories}</p>
+            <img src={image} alt=""/>
+        </div>
+
+    )
+
+}
+export default recipe
